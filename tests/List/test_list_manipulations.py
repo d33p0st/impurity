2ebd01460a439cce7ba948e688c14e88c85ea040
d33p0st <d33p0st@gmail.com>
2024-09-24 15:32:19 +0530

    Updated TEsts

--- tests/List/test_list_manipulations.py
+++ tests/List/test_list_manipulations.py
@@ -60,3 +60,80 @@ def checkStack():
         stack = list.convertToStackWithCapacity(3)
     except ValueError:
         assert True
+
+def test_filter():
+    list = List([1, 2, "abc", "xyz", "23", 45])
+
+    assert list.filter(str) == List(["abc", "xyz", "23"])
+    assert list.filter(int) == List([1, 2, 45])
+
+def test_interleave():
+    list = List([1, 2, 3, 4 ,5])
+
+    assert list.interleave([10, 20, 30, 40, 50]) == List([1, 10, 2, 20, 3, 30, 4, 40, 5, 50])
+    assert list.interleave([10, 20, 30], [70, 80], [100, 300]) == List([1, 10, 70, 100, 2, 20, 80, 300])
+
+def test_work():
+    def even(x: int) -> bool:
+        return x%2 == 0
+    
+    list = List([1, 2, 3, 4, 5, 6, 7])
+    assert list.work(even) == List([False, True, False, True, False, True, False])
+    assert list.work(even, True) == List([2, 4, 6])
+
+def test_counter():
+    list = List([1, 2, 2, 3, 4, 5, 5])
+    assert list.counter == {1: 1, 2: 2, 3: 1, 4: 1, 5: 2}
+
+def test_remove_duplicates():
+    list = List([1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3])
+    list.remove_duplicates
+    assert list == List([1, 2, 3])
+
+def test_swap():
+    list = List([1, 2, 2, 3, 4, 5, 6])
+    list.swap(0, len(list) - 1)
+
+    assert list == List([6, 2, 2, 3, 4, 5, 1])
+
+def test_partition():
+    def even(x: int) -> bool:
+        return x%2 == 0
+    
+    list = List([1, 2, 3 ,4, 5, 6, 7])
+
+    a, b = list.partition(even)
+
+    assert a == List([2, 4, 6])
+    assert b == List([1, 3, 5, 7])
+
+def test_combinations():
+    from itertools import combinations
+
+    list = List([1, 2, 3, 4, 5])
+
+    assert list.combinations(2) == List(combinations(list, 2))
+
+def test_reverse():
+    list = List([1, 2, 3, 4, 5, 6])
+    list.reverse
+
+    assert list == List([6, 5, 4, 3, 2, 1])
+
+def test_isPalindrome():
+    list1 = List([1, 2, 1])
+    list2 = List([1, 1, 2])
+
+    assert list1.isPalindrome == True
+    assert list2.isPalindrome == False
+
+def test_group_anagrams():
+    list = List(['abc', 'bca', 'ate', 'eat', 'tea'])
+    anagrams = list.group_anagrams
+
+    assert ['abc', 'bca'] in anagrams
+    assert ['ate', 'eat', 'tea'] in anagrams
+
+def test_merge_sorted():
+    list = List([1, 2, 3, 4])
+    assert list.merge_sorted([2, 6, 1, 3], key=lambda x: x) == List([1, 1, 2, 2, 3, 3, 4, 6])
