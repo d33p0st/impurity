a0e26c37101e1494fb362942f43a29b1a61fa0b7
d33p0st <d33p0st@gmail.com>
2024-10-13 22:19:08 +0530

    add the remaining algorithms in the index

---
+++ pysrc/modstore/algorithms/__init__.py
@@ -0,0 +1,17 @@
+from .sorting import (
+    Sort,
+    SortObject,
+    merge,
+    heapify,
+    is_sorted,
+    merge_3
+)
+
+__all__ = [
+    "Sort",
+    "SortObject",
+    "merge",
+    "heapify",
+    "is_sorted",
+    "merge_3"
+]
